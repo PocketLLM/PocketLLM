abf9ac6825ee05948a7e1043cff3293405c5a585
Mr-Dark-debug <prashantc592114@gmail.com>
2025-10-29 18:26:14 +0530

    chore(deps): remove sqlite3_flutter_libs plugin and clean up dependencies
    - Removed sqlite3_flutter_libs plugin from linux and windows Flutter plugin registrants
    - Deleted sqlite3_flutter_libs from generated_plugins.cmake files for linux and windows
    - Removed sqlite3_flutter_libs and related packages from pubspec.lock and pubspec.yaml
    - Updated dependencies in pubspec.yaml to remove explicit versions and deprecated entries
    - Added badge link to README.md title for PocketLLM project branding enhancement
    - Updated Dart and Flutter SDK version constraints in pubspec.lock to latest versions

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,10 +13,7 @@ import flutter_secure_storage_macos
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
-import sqflite_darwin
-import sqlite3_flutter_libs
 import url_launcher_macos
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
@@ -27,8 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
