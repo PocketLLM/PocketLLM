d5ed9fdb509e66eb239ad1672051344d81767ce9
Prashant  Choudhary <81732236+Mr-Dark-debug@users.noreply.github.com>
2025-10-31 17:26:53 +0530

    Use hooks_riverpod import for onboarding controller

--- lib/onboarding/onboarding_state.dart
+++ lib/onboarding/onboarding_state.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final onboardingControllerProvider =
